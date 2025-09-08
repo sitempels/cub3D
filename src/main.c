ec5f69a17fa2826b945ba15982a1bba87b66ec6e
asylum9 <agaland@student.s19.be>
2025-09-08 17:04:06 +0200

    src/main.c modif

--- src/main.c
+++ src/main.c
@@ -3,14 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stempels <stempels@student.s19.be>         +#+  +:+       +#+        */
+/*   By: agaland <agaland@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 13:36:10 by stempels          #+#    #+#             */
-/*   Updated: 2025/09/08 13:37:35 by stempels         ###   ########.fr       */
+/*   Updated: 2025/09/08 16:53:47 by agaland          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int argc, char **argv)
+#include "../inc/cub3D.h"
+
+bool	check_file_ext(char *filename, char *ext)
 {
-	return (0);
+	if (ft_strnstr(filename, ext, ft_strlen(filename)) != NULL)
+		return (true);
+	return true;
+}
+
+int	main(int ac, char **av)
+{
+	//t_game *game;
+	
+	if (ac != 2)
+	{
+		ft_printf_fd(STDERR_FILENO, "Wrong number of arguments\nUsage : ./cub3d <filename.cub>");
+		return (1);
+	}
+	
+	return 0;
+
 }
