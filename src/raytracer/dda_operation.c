efbf07d65ec3034835b2c556c0ad39e626e9d4e2
Simon Tempels <stempels@shi-r12-p1.s19.be>
2025-09-10 18:41:04 +0200

    dda in progress... show some ray now

--- src/raytracer/dda_operation.c
+++ src/raytracer/dda_operation.c
@@ -6,7 +6,7 @@
 /*   By: stempels <stempels@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 16:24:02 by stempels          #+#    #+#             */
-/*   Updated: 2025/09/10 16:03:39 by stempels         ###   ########.fr       */
+/*   Updated: 2025/09/10 18:38:22 by stempels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static double	dda_init(t_game *game, double dir[2], double plane[2], int *side);
 static double	get_first_dist(t_game *game, double raydir[2], int map[2], int *side, double d_dist[2]);
 static double	collision_dist(t_game *game, double d_dist[2], double side_dist[2], int *side, int step[2], int map[2]);
-static void	draw_line(t_game *game, double dist, double facing);
+static void	draw_line(t_game *game, double dist, double raydir[2], double d_dist[2], int side);
 
 double	dda_operation(t_game *game, double facing)
 {
@@ -27,11 +27,11 @@ double	dda_operation(t_game *game, double facing)
 //	double	old_time;
 
 	if (0 < facing && facing < M_PI)
-		dir[0] = 1;
+		dir[0] = -1;
 	else if (facing == 0 || facing == M_PI)
 		dir[0] = 0;
 	else
-		dir[0] = -1;
+		dir[0] = 1;
 	if (M_PI / 2 < facing && facing < 3 * M_PI / 2)
 		dir[1] = -1;
 	else if (facing == M_PI / 2 || facing == 3 * M_PI / 2)
@@ -44,7 +44,6 @@ double	dda_operation(t_game *game, double facing)
 	//time = 0;
 	//old_time = 0;
 	wall_dist = dda_init(game, dir, plane, &side);
-	draw_line(game, wall_dist, facing);
 	return (0);
 }
 static double	dda_init(t_game *game, double dir[2], double plane[2], int *side)
@@ -62,16 +61,24 @@ static double	dda_init(t_game *game, double dir[2], double plane[2], int *side)
 		camera_x = (2 * x) / ((double)game->max_x - 1);
 		raydir[0] = dir[0] + plane[0] * camera_x;
 		raydir[1] = dir[1] + plane[1] * camera_x;
-		map[0] = (int)game->player->pos[x];
+		map[0] = (int)game->player->pos[0];
 		if (raydir[0] == 0)
-			d_dist[0] = INT_MAX;
+		{
+			//raydir[0] = 2000;//
+			d_dist[0] = 2000;
+		}
 		else
 			d_dist[0] = fabs(1 / raydir[0]);
+		map[1] = (int)game->player->pos[1];
 		if (raydir[1] == 0)
-			d_dist[1] = INT_MAX;
+		{
+			//raydir[1] = 2000;
+			d_dist[1] = 2000;
+		}
 		else
 			d_dist[1] = fabs(1 / raydir[1]);
 		wall_dist = get_first_dist(game, raydir, map, side, d_dist);
+		draw_line(game, wall_dist, raydir, d_dist, *side);
 		x++;
 	}
 }
@@ -106,11 +113,12 @@ static double	collision_dist(t_game *game, double d_dist[2], double side_dist[2]
 	int		hit;
 
 	hit = 0;
-	i = 0;
-	while (hit == 0 && (game->max_x < map[0] && game->max_y < map[1]))
+	while (hit == 0 && (game->max_x > map[0] && game->max_y > map[1]))
 	{
 		if (side_dist[0] >= side_dist[1])
 			i = 1;
+		else
+			i = 0;
 		side_dist[i] += d_dist[i];
 		map[i] += step[i];
 		*side = i;
@@ -120,24 +128,24 @@ static double	collision_dist(t_game *game, double d_dist[2], double side_dist[2]
 	return (side_dist[i] - d_dist[i]);
 }
 
-static void	draw_line(t_game *game, double dist, double facing)
+static void	draw_line(t_game *game, double dist, double raydir[2], double d_dist[2], int side)
 {
 	int	x;
 	int	y;
-	float	dist_x;
-	float	dist_y;
+	double	dist_x;
+	double	dist_y;
 
-	dist_x = sinf(facing) * dist *64;
-	dist_y = cosf(facing) * dist *64;
-	x = game->player->pos[0];
-	y = game->player->pos[1];
-	while (x < dist_x || y < dist_y)
+	x = game->player->pos[0] * SIZE_MOD;
+	y = game->player->pos[1] * SIZE_MOD;
+	dist_x = 0;
+	dist_y = 0;
+	while ((side == 0 && dist_x < dist * SIZE_MOD) || (side == 1 && dist_y < dist * SIZE_MOD))
 	{
-		px_put(game->data, x, y, 0x74a33e);
-		if (x < y && x < dist_x)
-			x += dist_x / 100;
+		px_put(game->data, x - (dist_x * raydir[0]), y - (dist_y * raydir[1]), 0x74a33e);
+		if (raydir[0] != 0 && (dist_x < dist_y || raydir[1] == 0))
+			dist_x += d_dist[0];
 		else
-			y += dist_y / 100;
+			dist_y += d_dist[1];
 	}
 	mlx_put_image_to_window(game->data->mlx, game->data->win, game->data->img, 0, 0);
 }
